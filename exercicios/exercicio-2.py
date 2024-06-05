38d571ebb51c67660dc21d9aa2c6d64bdf727d67
kaique-cadimiel-dev <kaiqueamasio@gmail.com>
2024-06-04 22:56:47 -0300

    feat: cria função para descobrir a média em uma lista

---
+++ exercicios/exercicio-2.py
@@ -0,0 +1,6 @@
+# 🚀 Exercício 2: Calcule a média aritmética dos valores contidos em uma lista.
+
+def calc_average(list):
+    return sum(list) / len(list)
+
+print('average: ', calc_average([2, 3, 3, 5, 7, 10]))
